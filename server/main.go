d1fd18da056c4bcce9aaca37e249c8bc3c65cb3a
Lucas <lucasdev2124@gmail.com>
2024-06-12 23:50:21 -0300

    add user login and user creation

--- server/main.go
+++ server/main.go
@@ -12,16 +12,34 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/lucas271/DebateApp/internal/database"
 	"github.com/lucas271/DebateApp/utils"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type apiConfig struct {
 	DB *database.Queries
 }
 
-func main() {
+type userParams struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Name     string `json:"name"`
+}
 
+type defaultResp struct {
+	response  any
+	isSuccess bool
+}
+
+type userResp struct {
+	email string
+	name  string
+	id    string
+}
+
+func main() {
 	//load dotenv
 	err := godotenv.Load(".env")
+
 	if err != nil {
 		println("It was not possible to get enviroment variables, this is the error: ", err.Error())
 		return
@@ -33,7 +51,8 @@ func main() {
 		fmt.Println(err.Error())
 		return
 	}
-	http.HandleFunc("POST /testing", apiCfg.addUser)
+	http.HandleFunc("POST /createUser", apiCfg.createUser)
+	http.HandleFunc("POST /loginUser", apiCfg.loginUser)
 
 	if err := http.ListenAndServe(":37650", nil); err != nil {
 		fmt.Println(err.Error())
@@ -59,17 +78,11 @@ func connectToDB() (apiCfg apiConfig, err error) {
 		DB: queries,
 	}
 
-	return
+	return apiCfg, err
 }
 
-func (apiCfg *apiConfig) addUser(w http.ResponseWriter, r *http.Request) {
+func (apiCfg *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
 	//get info here
-
-	type userParams struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-		Name     string `json:"name"`
-	}
 
 	user := userParams{}
 	err := json.NewDecoder(r.Body).Decode(&user)
@@ -77,18 +90,66 @@ func (apiCfg *apiConfig) addUser(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		utils.JsonErr(w, 400, "invalid request body")
 	}
-	fmt.Printf("user %s", user.Email)
+
+	hashedPassword, hashErr := bcrypt.GenerateFromPassword([]byte(user.Password), 12)
+
+	if hashErr != nil {
+		utils.JsonErr(w, 400, "invalid password")
+	}
 
 	queryResp := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
 		Name:     user.Name,
 		Email:    user.Email,
-		Password: user.Password,
+		Password: string(hashedPassword),
 	})
-
 	if queryResp != nil {
 
 		println(queryResp.Error())
 		utils.JsonErr(w, 500, "Error creating user")
 	}
+	createdUser, err := apiCfg.DB.GetUser(r.Context(), user.Email)
+	if err != nil {
+		utils.JsonErr(w, 500, "User Was created but we were unable to get its info")
+	}
 
+	utils.JsonResp(w, 200, defaultResp{
+		response: userResp{
+			email: createdUser.Email,
+			name:  createdUser.Name,
+			id:    createdUser.ID,
+		},
+		isSuccess: true,
+	})
+}
+
+func (apiCfg *apiConfig) loginUser(w http.ResponseWriter, r *http.Request) {
+	user := userParams{}
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		utils.JsonErr(w, 400, "invalid request")
+	}
+
+	queryResp, err := apiCfg.DB.GetUser(r.Context(), user.Email)
+
+	if err != nil {
+		println(queryResp.Password)
+		println(err.Error())
+		utils.JsonErr(w, 400, err.Error())
+	}
+
+	passwordErr := bcrypt.CompareHashAndPassword([]byte(queryResp.Password), []byte(user.Password))
+	if passwordErr != nil {
+		utils.JsonErr(w, 400, "Password is not valid")
+	}
+
+	utils.JsonResp(w, 200, defaultResp{
+		response: userResp{
+			email: queryResp.Email,
+			name:  queryResp.Name,
+			id:    queryResp.ID,
+		},
+		isSuccess: true,
+	})
 }
--- server/sql/schema/001_users.sql
+++ server/sql/schema/001_users.sql
@@ -1,8 +1,8 @@
 -- +goose Up
 create table users(
     id varchar(200) primary key default(uuid()),
-    name varchar(60) not null,
-    email varchar(120) not null,
+    name varchar(60) not null UNIQUE,
+    email varchar(120) not null UNIQUE,
     password varchar(60) not null,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
